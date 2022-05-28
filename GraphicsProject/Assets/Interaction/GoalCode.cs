1334fcefaf6e338b7d011c32156dea0fa8a4e1eb
SinonHecateII <61343001+SinonHecateII@users.noreply.github.com>
2022-05-28 15:15:01 +0900

    Added Goal
    Need Make Canvas -> Animator(Canvas[Interaction]), Raw Image(GoalImage)
    AllViewCamera

---
+++ GraphicsProject/Assets/Interaction/GoalCode.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalCode : MonoBehaviour
+{
+    GameObject player;
+    public GameObject GoalCanvas;
+    public GameObject cam1;
+    public GameObject cam2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject == player) {
+            GoalCanvas.SetActive(true);
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+
+            Invoke("Disable", 2f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject == player) {
+            GoalCanvas.SetActive(false);
+        }
+    }
+    
+    void Disable() { 
+        GoalCanvas.SetActive(false);
+    }
+}
